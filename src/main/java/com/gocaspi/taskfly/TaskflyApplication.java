248f2a3bfa630ff71359a60abcb2336a3529fafc
Luca Wolckenhauer <l.wolckenhauer@gocaspi.de>
2022-10-18 14:49:57 +0200

    created test endpoint for docker tests

--- src/main/java/com/gocaspi/taskfly/TaskflyApplication.java
+++ src/main/java/com/gocaspi/taskfly/TaskflyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskflyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskflyApplication.class, args);
 	}
