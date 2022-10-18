a9414c02bdd8170ed9f41eafe4f66c0697389d6e
Luca Wolckenhauer <l.wolckenhauer@gocaspi.de>
2022-10-18 11:48:53 +0200

    initial commit

---
+++ src/test/java/com/gocaspi/taskfly/TaskflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gocaspi.taskfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
