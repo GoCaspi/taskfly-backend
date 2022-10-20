0f10e25f017e7605b00581520d262f5d7d8dd1eb
Luca Wolckenhauer <l.wolckenhauer@gocaspi.de>
2022-10-20 09:10:22 +0200

    updated dockerfile

--- src/main/java/com/gocaspi/taskfly/TaskflyApplication.java
+++ src/main/java/com/gocaspi/taskfly/TaskflyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskflyApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TaskflyApplication.class, args);
 	}
