271d408d770a63c28771e495e20f99dc37d9e885
nil <n.spiller@gocaspi.de>
2022-10-25 10:30:59 +0200

    feature/ON4T-174_Backend_for_Tasks - first unit test for TaskController.getAllTasks - mocking structure

---
+++ src/test/java/com/gocaspi/taskfly/Task/TaskControllerTest.java
@@ -0,0 +1,85 @@
+package com.gocaspi.taskfly.Task;
+
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class TaskControllerTest {
+
+	@Mock
+	private TaskRepository repository;
+
+	@InjectMocks
+	@Resource
+	private TaskController testedController;
+
+	@Before
+	public void setUp() throws Exception {
+		// Initialize mocks created above
+		MockitoAnnotations.initMocks(this);
+		// Change behaviour of `resource`
+	//	when(testedController.getSomething()).thenReturn("Foo");
+	}
+/*
+	@MockBean
+	private TaskRepository repository;
+
+	@Before
+	public void init() {
+		String[] mockUserIds =new String[] {"1", "2", "3"};
+		String mockListId = new String("1");
+		String mockTopic = new String("topic1");
+		String mockTeam = new String("team1");
+		String mockPrio = new String("prio1");
+		String mockDesc = new String("desc1");
+		String mockDeadline = new String("11-11-2022");
+		Task mockTask = new Task(mockUserIds,mockListId, mockTopic,mockTeam,mockPrio,mockDesc,mockDeadline);
+
+		//error, can't resolve method thenReturn(book)?
+		when(repository.findAll()).thenReturn((List<Task>) mockTask);
+
+	}
+
+ */
+
+	@Test
+	public void getAllTasks() {
+		TaskRepository mockRepo = mock(TaskRepository.class);
+		String[] mockUserIds =new String[] {"1", "2", "3"};
+		String mockListId = new String("1");
+		String mockTopic = new String("topic1");
+		String mockTeam = new String("team1");
+		String mockPrio = new String("prio1");
+		String mockDesc = new String("desc1");
+		String mockDeadline = new String("11-11-2022");
+		Task mockTask = new Task(mockUserIds,mockListId, mockTopic,mockTeam,mockPrio,mockDesc,mockDeadline);
+		Task[] mockTaskArr = new Task[]{mockTask,mockTask};
+
+
+		when(mockRepo.findAll()).thenReturn(List.of(mockTaskArr));
+		TaskController t = new TaskController(mockRepo); // TODO Replace default value.
+
+
+		String id = "1"; // TODO Replace default value.
+		Task[] expected = new Task[]{mockTask,mockTask,null};; // TODO Replace default value.
+		String expectedOutput= new Gson().toJson(expected);
+		String actual = t.getAllTasks(id);
+
+		assertEquals(expectedOutput, actual);
+	}
+
+
+}
