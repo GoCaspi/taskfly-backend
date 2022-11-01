6209e202a7376b22d4dd22fc513633c617d681b3
nil <n.spiller@gocaspi.de>
2022-11-01 14:42:15 +0100

    feature/ON4T-1feature/ON4T-174_Backend_for_Tasks_Tests - added all tests for the Task class

--- src/test/java/com/gocaspi/taskfly/Task/TaskTest.java
+++ src/test/java/com/gocaspi/taskfly/Task/TaskTest.java
@@ -25,12 +25,35 @@ public class TaskTest {
 			new Testcase_setString(""),
 	};
 
+	class Testcase_getString{
+		final String expected;
+
+		Testcase_getString(String expected) {
+			this.expected = expected;
+		}
+	}
+
+	Testcase_getString[] testcases_get = new Testcase_getString[]{
+			new Testcase_getString("abc"),
+			new Testcase_getString(null),
+			new Testcase_getString(""),
+	};
+
 	@Test
 	public void setDescription() {
 		Task t = mockTask;
 		for (Testcase_setString tc : testcases){
 			t.setDescription(tc.newText);
 			assertEquals(t.getDescription(),tc.newText);
+		}
+	}
+
+	@Test
+	public void setUserId() {
+		Task t = mockTask;
+		for (Testcase_setString tc : testcases){
+			t.setUserId(tc.newText);
+			assertEquals(t.getUserId(),tc.newText);
 		}
 	}
 
@@ -69,4 +92,83 @@ public class TaskTest {
 			assertEquals(t.getDeadline(),tc.newText);
 		}
 	}
+	@Test
+	public void getDescription(){
+		Task t = mockTask;
+		for (Testcase_getString tc : testcases_get){
+			t.setDescription(tc.expected);
+			String actual = t.getDescription();
+			assertEquals(actual,tc.expected);
+		}
+	}
+
+	@Test
+	public void getDeadline(){
+		Task t = mockTask;
+		for (Testcase_getString tc : testcases_get){
+			t.setDeadline(tc.expected);
+			String actual = t.getDeadline();
+			assertEquals(actual,tc.expected);
+		}
+	}
+
+	@Test
+	public void getPriority(){
+		Task t = mockTask;
+		for (Testcase_getString tc : testcases_get){
+			t.setPriority(tc.expected);
+			String actual = t.getPriority();
+			assertEquals(actual,tc.expected);
+		}
+	}
+
+	@Test
+	public void getTeam(){
+		Task t = mockTask;
+		for (Testcase_getString tc : testcases_get){
+			t.setTeam(tc.expected);
+			String actual = t.getTeam();
+			assertEquals(actual,tc.expected);
+		}
+	}
+
+	@Test
+	public void getTopic(){
+		Task t = mockTask;
+		for (Testcase_getString tc : testcases_get){
+			t.setTopic(tc.expected);
+			String actual = t.getTopic();
+			assertEquals(actual,tc.expected);
+		}
+	}
+
+	@Test
+	public void getTaskIdString(){
+		Task t = mockTask;
+		for (Testcase_getString tc : testcases_get){
+			t.setTopic(tc.expected);
+			String actual = t.getTaskIdString();
+			assertEquals(actual,t.getTaskIdString());
+		}
+	}
+
+	@Test
+	public void getListId(){
+		Task t = mockTask;
+		for (Testcase_getString tc : testcases_get){
+			t.setListId(tc.expected);
+			String actual = t.getListId();
+			assertEquals(actual,tc.expected);
+		}
+	}
+
+	@Test
+	public void setListId() {
+		Task t = mockTask;
+		for (Testcase_setString tc : testcases){
+			t.setListId(tc.newText);
+			assertEquals(t.getListId(),tc.newText);
+		}
+	}
+
 }
