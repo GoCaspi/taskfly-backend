a64ad7f2461d9d15ccc50ddafc26878f06ba32c2
nil <n.spiller@gocaspi.de>
2022-11-01 17:12:54 +0100

    feature/ON4T-1feature/ON4T-174_Backend_for_Tasks_Documentation_TaskController - corrected the documentation of the TaskController

--- src/main/java/com/gocaspi/taskfly/Task/TaskController.java
+++ src/main/java/com/gocaspi/taskfly/Task/TaskController.java
@@ -23,15 +23,22 @@ public class TaskController {
        this.service = new TaskService(repository);
    }
 
+    /**
+     * returns the service  of type TaskService
+     *
+     * @return TaskService that is injected in the Controller
+     */
     public TaskService getService() {
         return this.service;
     }
 
-
     /**
      * given a request body this endpoint converts the body to a Task and validates the input Data against set criteria (see method below)
-     * If criteria are matched returns status 200 OK, else throws an exception
-     * @param body, request body
+     * If criteria are matched returns HttpStatus:202, else throws an exception.
+     *
+     * @param body json of the task that should be created
+     * @return ResponseEntity containing a success message along with the http status code
+     * @throws ChangeSetPersister.NotFoundException Exception if no task to the id was found
      */
     @PostMapping
     public ResponseEntity<String> Handle_createNewTask(@RequestBody String body) throws ChangeSetPersister.NotFoundException {
@@ -42,10 +49,13 @@ public class TaskController {
     }
 
     /**
-     * calls the db via TaskRepository interface. If no tasks are assigned to the provided id an exception is thrown,
-     * else return an ArrayList with all tasks to that id as a json
-     * @param id of the user
-     * @return String, json of all tasks or err
+     * calls the service to fetch all tasks assigned to the provided Pathvariable id (userId). If the List of tasks fetched from the service
+     * has a length of zero, there are no tasks assigned to the provided id and an exception is thrown. Else a List with the tasks of the user
+     * is returned along with the HttpStatus:200.
+     *
+     * @param id id of the user that want to fetch all his tasks
+     * @return ResponseEntity containing the list with all tasks to the given id along with the http status code
+     * @throws ChangeSetPersister.NotFoundException Exception if no task to the id was found
      */
     @GetMapping("/userId/{id}")
     public ResponseEntity<List<Task>> Handle_getAllTasks(@PathVariable String id) throws ChangeSetPersister.NotFoundException {
@@ -54,6 +64,14 @@ public class TaskController {
         return new ResponseEntity<>(tasks, HttpStatus.OK);
     }
 
+    /**
+     * calls the service to fetch the task of the provided id. If the service does not throw an exception (no task to the provided id was found)
+     * the task to the given id is returned along with a HttpStatus:200, else an exception is thrown.
+     *
+     * @param id id of the task
+     * @return ResponseEntity, containing the task from the db and the http status code
+     * @throws ChangeSetPersister.NotFoundException Exception if no task to the id was found
+     */
     @GetMapping("/taskId/{id}")
     public ResponseEntity<Task> Handle_getTaskById(@PathVariable String id) throws ChangeSetPersister.NotFoundException {
         Task task = getService().getService_TaskById(id);
@@ -62,6 +80,7 @@ public class TaskController {
 
     /**
      * if there is a task to the provided id (path variable) then that task is removed from the mongoDB, else an exception is thrown
+     *
      * @param id, identifier of the task of intereset
      */
     @DeleteMapping("/{id}")
@@ -72,13 +91,15 @@ public class TaskController {
     }
 
     /**
-     * to a given path-variable id , search the injected repository if there is a task to that provided id.
-     * If that holds true, checks which fields of the request-body (body) are not null and therefore should be updated, then saves
-     * the updated task to the repository.
-     * Else throws an exception.
-     * @param id, id of the task that should be updated
-     * @param body, update for the task with the id id
-     * @return String, message if update process was successfull
+     * Decodes the Requestbody into a task, which will be the update to the existing task assigned to the Pathvariable id.
+     * Then the method calls the service to validate that there is a task assigned to the provided id and update that task with the Requestbody.
+     * If the service doesnt throw an exception then a succesmessage with the updated task id and HttpStatus:202 will be returned,
+     * else the exception from the service is thrown.
+     *
+     * @param id id of the task that should be updated
+     * @param body update of the task to the provided id
+     * @return ResponseEntity containing success message and updated task id and the http status code
+     * @throws ChangeSetPersister.NotFoundException Exception if no task to the id was found
      */
     @PutMapping("/{id}")
     public ResponseEntity<String> Handle_updateTask(@PathVariable String id,@RequestBody String body) throws ChangeSetPersister.NotFoundException {
@@ -91,6 +112,7 @@ public class TaskController {
 
     /**
      * given a requestbody (Json of a Task) the method checks if all fields are null-safe with the exception of the fields: priority and deadline, which must not be set.
+     *
      * @param jsonPayload, request body
      * @return true if the mentioned criteria holds for that Task-payload, else return false
      */
@@ -100,10 +122,10 @@ public class TaskController {
     }
 
     /**
-     * returns a Task from a json String
+     * returns a Task from a Json
+     *
      * @param jsonPayload String
-     * @return Task
+     * @return Task fetched from the jsonPayload
      */
     public Task jsonToTask(String jsonPayload){ return new Gson().fromJson(jsonPayload, Task.class);}
-
 }
