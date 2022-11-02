9b12e316940a9eb5b0d6f883d7ff5c6a65747c7f
nil <n.spiller@gocaspi.de>
2022-11-02 14:04:13 +0100

    feature/ON4T-1feature/ON4T-174_Backend_for_Tasks_Tests_TaskService - bash script for sonarqube

--- scripts/sonarscan.sh
+++ scripts/sonarscan.sh
@@ -1,2 +1,6 @@
-jest --coverage
-node sonarqube-scanner.js
+#!/bin/bash
+source /etc/profile.d/maven.sh
+cd ..
+mvn clean
+mvn test
+mvn -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco-ut/jacoco.xml sonar:sonar -Pcoverage
