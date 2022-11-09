aa7fc19c479e145e424e426e898a9b166c746da5
SAM <s.armin@gocaspi.de>
2022-11-09 16:25:47 +0100

    Added TeamManagement

--- src/main/java/com/gocaspi/taskfly/teammanagement/TeamManagementController.java
+++ src/main/java/com/gocaspi/taskfly/teammanagement/TeamManagementController.java
@@ -1,19 +1,18 @@
 package com.gocaspi.taskfly.teammanagement;
 
-import com.gocaspi.taskfly.teammanagement.TeamManagementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.google.gson.Gson;
 import org.springframework.web.client.HttpClientErrorException;
-import java.util.List;
+
 
 @RestController
 @ResponseBody
 @RequestMapping("/teammanagement")
 public class TeamManagementController {
-    @Autowired
+    @Autowired //teilt spring mit wo es Objekte in anderen klassen anlegen soll
     private TeamManagementRepository repository;
 
     private final TeamManagementService service;
@@ -27,4 +26,17 @@ public class TeamManagementController {
     public TeamManagementService getService() {
         return service;
     }
+
+    @PostMapping("/createTeam")
+    public ResponseEntity<String> createTeam(@RequestBody String body) throws HttpClientErrorException.BadRequest {
+        TeamManagement teamManagement = jsonToTeamManagement(body);
+        getService().postService(teamManagement);
+        String msg = "successfully created Team";
+        return new ResponseEntity<>(msg, HttpStatus.ACCEPTED);
+    }
+
+    public TeamManagement jsonToTeamManagement(String jsonPayload){
+        return new Gson().fromJson(jsonPayload, TeamManagement.class);
+    }
+
 }
--- src/main/java/com/gocaspi/taskfly/teammanagement/TeamManagementService.java
+++ src/main/java/com/gocaspi/taskfly/teammanagement/TeamManagementService.java
@@ -1,8 +1,6 @@
 package com.gocaspi.taskfly.teammanagement;
 
-import com.gocaspi.taskfly.teammanagement.TeamManagementRepository;
 import com.google.gson.Gson;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -11,17 +9,32 @@ import org.springframework.web.client.HttpClientErrorException;
 import java.util.*;
 public class TeamManagementService {
     @Autowired
-    private TeamManagementRepository repo;
-
-    public TeamManagementService(TeamManagementRepository repo){
-        this.repo = repo;
+    private TeamManagementRepository repository;
+    private final HttpClientErrorException exceptionNotFound;
+    private final HttpClientErrorException exceptionBadRequest;
+    public TeamManagementService(TeamManagementRepository repository){
+        this.repository = repository;
         this.exceptionNotFound = HttpClientErrorException.create(HttpStatus.NOT_FOUND, "not found", new HttpHeaders(), "".getBytes(),null);
         this.exceptionBadRequest = HttpClientErrorException.create(HttpStatus.NOT_FOUND, "bad payload", new HttpHeaders(), "".getBytes(), null);
     }
-    private final HttpClientErrorException exceptionNotFound;
+    public TeamManagementRepository getRepository() { return repository; }
+    public HttpClientErrorException getNotFound() { return exceptionNotFound; }
 
-    private final HttpClientErrorException exceptionBadRequest;
+    public void postService(TeamManagement t) throws HttpClientErrorException {
+        if(!validateTaskFields(new Gson().toJson(t))){
+            throw exceptionBadRequest;
+        }
+        getRepository().insert(t);
+    }
 
-    public HttpClientErrorException getNotFound(){return this.exceptionNotFound;}
+    public boolean validateTaskFields(String jsonPayload){
+        TeamManagement teamManagement = jsonToTeamManagement(jsonPayload);
+
+        return !Objects.equals(teamManagement.getTeamID(), null) && !Objects.equals(teamManagement.getObjectId(), null ) &&
+                !Objects.equals(teamManagement.getTeamName(), null) && !Objects.equals(teamManagement.getUserID(), null);
+    }
+    public TeamManagement jsonToTeamManagement(String jsonPayload){
+        return new Gson().fromJson(jsonPayload, TeamManagement.class);
+    }
 
 }
