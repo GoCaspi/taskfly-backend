92e2214a4663e7bdb03848df419e7b7776ce4729
nil <n.spiller@gocaspi.de>
2022-11-29 11:11:26 +0100

    feature/ON4T-261_ResetPassword_Tests: - all test for resetService

---
+++ src/test/java/com/gocaspi/taskfly/reset/ResetControllerTest.java
@@ -0,0 +1,16 @@
+package com.gocaspi.taskfly.reset;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ResetControllerTest {
+	@Test
+	public void handleSetNewUserPwd() {
+		ResetController r = null; // TODO Replace default value.
+		String body = null; // TODO Replace default value.
+		ResponseEntity expected = null; // TODO Replace default value.
+		ResponseEntity actual = r.handleSetNewUserPwd(body);
+
+		assertEquals(expected, actual);
+	}
+}
